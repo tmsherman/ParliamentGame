a0cc1df4a91257440369ade1ddb251e0e7044556
Timothy Sherman <tmsherma@gmail.com>
2016-11-29 11:10:37 -0500

    Changed Yaml Format, load Yaml

---
+++ Assets/moveDownScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveDownScript : MonoBehaviour {
+
+	public bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!activated)
+			return;
+		transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+	}
+
+	public void Activate() {
+		activated = true;
+	}
+}
