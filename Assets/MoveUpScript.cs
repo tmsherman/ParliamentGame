e1b5f3bb615d491f4b7b6094ebb6ec753b4db6f8
kensitu <kensit2@gmail.com>
2016-12-12 05:30:35 -0500

    Additional UI Elements
    Added UI elements for Resources, Button hover, Visitor

--- Assets/MoveUpScript.cs
+++ Assets/MoveUpScript.cs
@@ -1,22 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class moveDownScript : MonoBehaviour {
+public class moveUpScript : MonoBehaviour {
 
 	public bool activated = false;
 
-	private GameObject guyINeedToTouchOrReferenceOrWhatever;
+	private GameObject mage;
 
 	// Use this for initialization
 	void Start () {
-		guyINeedToTouchOrReferenceOrWhatever = GameObject.Find ("Mage");
+		mage = GameObject.Find ("Mage");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (!activated)
 			return;
-		if (guyINeedToTouchOrReferenceOrWhatever.transform.position.y < 0)
+		else if (mage.transform.position.y < 1.56)
 			transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
 	}
 
