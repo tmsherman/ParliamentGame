ba68ae1abd4b90d7c3fd7e517cbc7da113f12889
kensitu <kensit2@gmail.com>
2016-12-13 16:36:23 -0500

    New Unity Resolution, Resources UI Elements, Animation Script for Visitor

--- Assets/moveDownScript.cs
+++ Assets/moveDownScript.cs
@@ -7,22 +7,29 @@ public class moveDownScript : MonoBehaviour {
 
 	private GameObject visitor;
 
+	//Animation variables
+	private Animator anim;
+	private int leaveHash = Animator.StringToHash("leave");
+
 	// Use this for initialization
 	void Start () {
 		visitor = GameObject.Find ("Visitor");
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!activated)
 			return;
-		if (visitor.transform.position.y < 1F)
+		if (visitor.transform.position.y < 1F) {
 			transform.position = new Vector3 (transform.position.x, transform.position.y + 0.04F, transform.position.z);
+		}
 		else
 			activated = false;
 	}
 
 	public void Activate() {
+		anim.SetBool (leaveHash, false);
 		activated = true;
 	}
 }
