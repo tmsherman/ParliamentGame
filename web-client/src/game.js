4c9f7cf93d5e00151c9f0d04a480d94fb68dce5a
Cole Gleason <cole@colegleason.com>
2016-12-14 00:47:59 -0500

    web-client updates

--- web-client/src/game.js
+++ web-client/src/game.js
@@ -2,13 +2,24 @@ var fb = require('./firebase');
 var interface = require('./interface');
 var user = require('./user');
 
+var roleToResourceMap = {
+	'peasant': 'happiness',
+	'noble': 'power',
+	'merchant': 'wealth'
+};
+module.exports.roleToResourceMap = roleToResourceMap;
+
 var outcomesRef = fb.database().ref('/outcomes');
 var eventsRef = fb.database().ref('/events');
 var stateRef = fb.database().ref('/state');
 
 module.exports.loadCurrentState = function() {
 	outcomesRef.on('child_added', function(snap) {
-		interface.addOutcome(snap.val());
+		var outcome = snap.val();
+		var userRole = user.getUser().role;
+		if (userRole == outcome.type) {
+			interface.addOutcome(snap.val());
+		}
 	});
 	eventsRef.on('child_added', function(snap) {
 		interface.displayEvent(snap.key, snap.val());
@@ -18,11 +29,6 @@ module.exports.loadCurrentState = function() {
 
 function updateResources(snap) {
 	var state = snap.val();
-	var roleToResourceMap = {
-		'peasant': 'happiness',
-		'noble': 'power',
-		'merchant': 'wealth'
-	};
 	var userRole = user.getUser().role;
 	if (userRole) {
 		var desiredResource = roleToResourceMap[userRole];
