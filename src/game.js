b771c8bfdb38aebea80d7d50c9770a693dedcff8
Cole Gleason <cole@colegleason.com>
2016-12-14 02:59:04 -0500

    debug

--- src/game.js
+++ src/game.js
@@ -35,7 +35,7 @@ module.exports.loadCurrentState = function() {
 		var end = moment.unix(event.utctime)
 		  .add(config.eventDuration, 'seconds');
 		var now = moment.utc();
-		console.log(event, event.type == userRole, now < end);
+		console.log(event, now, end, now < end);
 		if (event.type == userRole && now < end) {
 			interface.displayEvent(snap.key, snap.val());
 		}
